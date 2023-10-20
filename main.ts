1fe53789bd249f23d0fd7df1d450d3f5be658d13
gabimaiello <105435046+gabimaiello@users.noreply.github.com>
2023-10-20 11:06:41 -0300

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,8 @@
+fuctiontocasom(SelectorAudio{
+  constelemento=document.querySelector(SelectorAudio);
+  if (elements&&elemento.localname===áudio){
+    elemento.play();
+  }
+  else{
+    console.log('elemento não encontrado ou seletor inválido');
+    for(let.contador=0;contador<ListaDeTeclas.length
